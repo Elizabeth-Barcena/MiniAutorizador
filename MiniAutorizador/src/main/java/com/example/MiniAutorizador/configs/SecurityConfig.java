0ab7fab47c0a81314f675687a4a4ba8b461bf7d6
Elizabeth-Barcena <beth_barcena@hotmail.com>
2026-01-20 18:34:26 -0300

    feat: Configuração do swagger para facilitar testes

--- MiniAutorizador/src/main/java/com/example/MiniAutorizador/configs/SecurityConfig.java
+++ MiniAutorizador/src/main/java/com/example/MiniAutorizador/configs/SecurityConfig.java
@@ -15,10 +15,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
